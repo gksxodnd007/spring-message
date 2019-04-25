3ead03958f95ff38d14e4034844b269f810f8c59
codingsquid <gksxodnd007@gmail.com>
2019-04-25 23:57:06 +0900

    [Feature] 빈스캐닝 범위 추가

--- producer/src/main/kotlin/org/mashup/backend/producer/ProducerApplication.kt
+++ producer/src/main/kotlin/org/mashup/backend/producer/ProducerApplication.kt
@@ -1,9 +1,10 @@
 package org.mashup.backend.producer
 
+import org.mashup.backend.common.CommonApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = [ProducerApplication::class, CommonApplication::class])
 class ProducerApplication
 
 fun main(args: Array<String>) {
